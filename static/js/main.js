935a3fba4d81bd6e8025ec32c0c87ce9a2b36713
Skagemo <5012227+sskagemo@users.noreply.github.com>
2023-01-19 08:14:44 +0100

    forsøk på å få til relativ adresse

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
-			.register('/sw.js')
+			.register('sw.js')
 			.then(reg => console.log('Service Worker: Registered'))
 			.catch(err => console.log(`Service Worker: Error: ${err}`));
 	});
